140f0ca0b4559deeee1c342a6f2920af96460c47
Erick Tijero <ericktijerou@gmail.com>
2020-05-18 13:01:15 -0500

    ⚙ Use ContextCompat to get resources

--- koleton-base/src/main/kotlin/koleton/util/Contexts.kt
+++ koleton-base/src/main/kotlin/koleton/util/Contexts.kt
@@ -1,22 +1,20 @@
 package koleton.util
 
 import android.content.Context
-import android.content.res.ColorStateList
-import android.graphics.Color
 import android.graphics.drawable.Drawable
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.annotation.ColorRes
 import androidx.annotation.DrawableRes
-import androidx.appcompat.content.res.AppCompatResources
+import androidx.core.content.ContextCompat
 
 internal fun Context.getDrawableCompat(@DrawableRes resId: Int): Drawable {
-    return checkNotNull(AppCompatResources.getDrawable(this, resId)) { "Invalid resource ID: $resId" }
+    return checkNotNull(ContextCompat.getDrawable(this, resId)) { "Invalid resource ID: $resId" }
 }
 
 internal fun Context.getColorCompat(@ColorRes resId: Int): Int {
-    return checkNotNull(AppCompatResources.getColorStateList(this, resId).defaultColor) { "Invalid resource ID: $resId" }
+    return checkNotNull(ContextCompat.getColor(this, resId)) { "Invalid resource ID: $resId" }
 }
 
 internal fun Context.inflate(res: Int, parent: ViewGroup? = null) : View {
