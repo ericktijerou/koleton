8a6a9e1492ebe34e01aae83b9eada396122a81bb
Erick Tijero <ericktijerou@gmail.com>
2021-02-01 22:33:40 -0500

    Create Constants.kt

--- koleton-sample/src/main/kotlin/koleton/sample/utils/Extensions.kt
+++ koleton-sample/src/main/kotlin/koleton/sample/utils/Extensions.kt
@@ -26,7 +26,3 @@ fun getViewModelFactory(): ViewModelFactory {
     val executor = Executors.newFixedThreadPool(5)
     return ViewModelFactory(JourneyRepository(executor))
 }
-
-const val DEFAULT_DELAY: Long = 3000
-const val DEFAULT_PAGE_SIZE: Int = 10
-const val ITEM_COUNT: Int = 3
